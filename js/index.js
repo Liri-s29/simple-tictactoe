3db8068d45c4de825382a7563fa10f75ab36bbfc
Srihari S <srihari.sundaramurugan@gmail.com>
2021-06-29 17:48:46 +0530

    final commit

--- js/index.js
+++ js/index.js
@@ -6,18 +6,18 @@ const o = document.querySelector(".o");
 const reset = document.querySelector(".restart");
 
 const selector = [x,o];
-let value = "X";
+let value = "O";
 
-selector.forEach((s)=>{
-    s.addEventListener("click", ()=>{
-        value = s.innerText;
-        console.log(value);
-    });
-});
+
 
 items.forEach((item)=>{
     item.addEventListener("click",()=>{
-        if(!item.innerText){
+        if(value == "O"){
+            value = "X";
+        }else{
+            value = "O";
+        }
+        if(item.innerText == ""){
             item.innerText = value;
         }
     });
@@ -26,6 +26,7 @@ items.forEach((item)=>{
 reset.addEventListener("click",()=>{
     items.forEach((item)=>{
         item.innerText = "";
+        value = "O";
     });
 });
 
